389f27170b4db2f93c2eef2241ad8a038541b0d3
Milan Thapa <milans.thapa78@gmail.com>
2017-12-03 07:33:59 +0000

    adding a decor, color brings joy 😂

--- init.el
+++ init.el
@@ -38,13 +38,16 @@
 (org-babel-load-file (concat user-emacs-directory "basics.org"))
 (org-babel-load-file (concat user-emacs-directory "utils.org"))
 (org-babel-load-file (concat user-emacs-directory "orgmode.org"))
+(org-babel-load-file (concat user-emacs-directory "ui.org"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (ag try use-package))))
+ '(package-selected-packages
+   (quote
+    (expand-region neotree company-emoji mode-icons solarized-theme doom-themes ag try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
