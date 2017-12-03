0305c0e39171877d773fd4bf2d4c8ec5680fdb76
Milan Thapa <milans.thapa78@gmail.com>
2017-12-03 12:01:39 +0545

    setting up basics

--- init.el
+++ init.el
@@ -22,12 +22,28 @@
         ("gnu" . "https://elpa.gnu.org/packages/")
         ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
-;; using use-package	   
+;; using use-package and basics
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
 (eval-when-compile
   (require 'use-package))
-(require 'diminish)                ;; if you use :diminish
+;; (require 'diminish)                ;; if you use :diminish
 (require 'bind-key)
+(use-package try
+  :ensure t)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (try use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
