4cdde78a6bf79eab4e90f71cd7d596ccecfd851c
thapakazi <milans.thapa78@gmail.com>
2019-01-10 21:26:14 +0545

    revisit - save configs from 2018

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; Begin initialization
+;; Begin initialization
 ;; Turn off mouse interface early in startup to avoid momentary display
 (when window-system
   (menu-bar-mode -1)
@@ -13,52 +13,145 @@
 ;;; copied_from: https://github.com/steckerhalter/steckemacs.el/blob/fb686004dd542246910b88efe61b2242536ad964/steckemacs.el#L31-L43
 ;; disable the GNU ELPA
 (setq package-archives nil)
-;; initialize the package system
+
+;; lets try melpa for a bit
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
-(if (require 'quelpa nil t)
-    (setq quelpa-update-melpa-p nil)
-  (with-temp-buffer
-    (url-insert-file-contents "https://raw.github.com/quelpa/quelpa/master/bootstrap.el")
-    (eval-buffer)))
-
-;;; Bootstrap use-package
-;; install use-package and the quelpa handler
-(quelpa
- '(quelpa-use-package
-   :fetcher github
-   :repo "quelpa/quelpa-use-package"))
-(require 'quelpa-use-package)
-
-;;; Bootstrap use-package
-;; Install use-package if it's not already installed.
-;; use-package is used to configure the rest of the packages.
-;; (unless (package-installed-p 'use-package)
-;;   (package-refresh-contents)
-;;   (package-install 'use-package))
+;; using use-package	   
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (eval-when-compile
   (require 'use-package))
-(require 'diminish)                ;; if you use :diminish
+;; (require 'diminish)                ;; if you use :diminish
 (require 'bind-key)
-                                        ;(setq use-package-verbose t)
+
+;; Add a directory to the load path so we can put extra files there
+(add-to-list 'load-path "~/.emacs.d/custom-load-list/")
 
 ;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "basics.org"))
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+;; (org-babel-load-file (concat user-emacs-directory "config.org"))
+(org-babel-load-file (concat user-emacs-directory "utils.org"))
+
+;; (org-babel-load-file (concat user-emacs-directory "settings.org"))
 (org-babel-load-file (concat user-emacs-directory "ror.org"))
 (org-babel-load-file (concat user-emacs-directory "org-mode.org"))
 (org-babel-load-file (concat user-emacs-directory "ui.org"))
-(org-babel-load-file (concat user-emacs-directory "go_lang.org"))
-(org-babel-load-file (concat user-emacs-directory "fun.org"))
+
+
+;; (org-babel-load-file (concat user-emacs-directory "go_lang.org"))
+
+;;(org-babel-load-file (concat user-emacs-directory "fun.org"))
+
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   [("#1B2229" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#DFDFDF")])
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(cursor-type (quote bar))
+ '(fci-rule-color "#5B6268")
+ '(global-linum-mode t)
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+   (--map
+	(solarized-color-blend it "#002b36" 0.25)
+	(quote
+	 ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+   (quote
+	(("#073642" . 0)
+	 ("#546E00" . 20)
+	 ("#00736F" . 30)
+	 ("#00629D" . 50)
+	 ("#7B6000" . 60)
+	 ("#8B2C02" . 70)
+	 ("#93115C" . 85)
+	 ("#073642" . 100))))
+ '(hl-bg-colors
+   (quote
+	("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
+ '(hl-fg-colors
+   (quote
+	("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
+ '(hl-paren-colors (quote ("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900")))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
+ '(js-indent-level 2)
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   (quote
+	("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+ '(org-agenda-files (quote ("~/org/cloudfactory/scratch.org")))
+ '(org-ellipsis "  ")
+ '(org-fontify-done-headline t)
+ '(org-fontify-quote-and-verse-blocks t)
+ '(org-fontify-whole-heading-line t)
  '(package-selected-packages
    (quote
-    (elfeed edit-server magit helm-ag helm-projectile helm c-eldoc ace-jump-mode ido-vertical-mode transpose-frame ox-pandoc solarized-theme use-package cyberpunk-theme))))
+	(solidity-mode nginx-mode edit-indirect which-key counsel-projectile dumb-jump multiple-cursors markdown-mode+ popup-kill-ring beacon htmlize ox-reveal ag swiper hydra yaml-mode smooth-scrolling smooth-scroll org-link-minor-mode elfeed projectile-rails pandoc ox-pandoc try expand-region hungry-delete org-bullets go-eldoc use-package)))
+ '(pdf-view-midnight-colors (quote ("#969896" . "#f8eec7")))
+ '(pos-tip-background-color "#073642")
+ '(pos-tip-foreground-color "#93a1a1")
+ '(recentf-mode t)
+ '(send-mail-function (quote smtpmail-send-it))
+ '(small-temporary-file-directory "/tmp/")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 25)
+ '(tab-width 4)
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(vc-annotate-background "#1B2229")
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   (list
+	(cons 20 "#98be65")
+	(cons 40 "#b4be6c")
+	(cons 60 "#d0be73")
+	(cons 80 "#ECBE7B")
+	(cons 100 "#e6ab6a")
+	(cons 120 "#e09859")
+	(cons 140 "#da8548")
+	(cons 160 "#d38079")
+	(cons 180 "#cc7cab")
+	(cons 200 "#c678dd")
+	(cons 220 "#d974b7")
+	(cons 240 "#ec7091")
+	(cons 260 "#ff6c6b")
+	(cons 280 "#cf6162")
+	(cons 300 "#9f585a")
+	(cons 320 "#6f4e52")
+	(cons 340 "#5B6268")
+	(cons 360 "#5B6268")))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+	(unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
+ '(winner-mode t)
+ '(xterm-color-names
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
+ '(xterm-color-names-bright
+   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -66,3 +159,15 @@
  ;; If there is more than one, they won't work right.
  )
 (put 'set-goal-column 'disabled nil)
+
+;; experimental 
+;; (require 'keyfreq)
+;; (setq keyfreq-excluded-commands
+;;       '(self-insert-command
+;;         abort-recursive-edit
+;;         forward-char
+;;         backward-char
+;;         previous-line
+;;         next-line))
+;; (keyfreq-mode 1)
+;; (keyfreq-autosave-mode 1)
