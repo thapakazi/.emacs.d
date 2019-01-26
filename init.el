497b91ffd52ec0859d815e552c55f69ea7bc8ba7
thapakazi <milans.thapa78@gmail.com>
2019-01-26 21:49:22 +0545

    org - fix blog generator, +-minor change
    minor changes everywhere
    yeah if you are reading this, big things coming ;)

--- init.el
+++ init.el
@@ -75,30 +75,29 @@
  '(cua-read-only-cursor-color "#859900")
  '(cursor-type (quote bar))
  '(fci-rule-color "#5B6268")
- '(global-linum-mode t)
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
  '(highlight-symbol-colors
    (--map
-	(solarized-color-blend it "#002b36" 0.25)
-	(quote
-	 ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+    (solarized-color-blend it "#002b36" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
  '(highlight-symbol-foreground-color "#93a1a1")
  '(highlight-tail-colors
    (quote
-	(("#073642" . 0)
-	 ("#546E00" . 20)
-	 ("#00736F" . 30)
-	 ("#00629D" . 50)
-	 ("#7B6000" . 60)
-	 ("#8B2C02" . 70)
-	 ("#93115C" . 85)
-	 ("#073642" . 100))))
+    (("#073642" . 0)
+     ("#546E00" . 20)
+     ("#00736F" . 30)
+     ("#00629D" . 50)
+     ("#7B6000" . 60)
+     ("#8B2C02" . 70)
+     ("#93115C" . 85)
+     ("#073642" . 100))))
  '(hl-bg-colors
    (quote
-	("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
+    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
  '(hl-fg-colors
    (quote
-	("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
+    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
  '(hl-paren-colors (quote ("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900")))
  '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
@@ -107,7 +106,7 @@
  '(magit-diff-use-overlays nil)
  '(nrepl-message-colors
    (quote
-	("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(org-agenda-files (quote ("~/org/cloudfactory/scratch.org")))
  '(org-ellipsis "  ")
  '(org-fontify-done-headline t)
@@ -115,7 +114,7 @@
  '(org-fontify-whole-heading-line t)
  '(package-selected-packages
    (quote
-	(solidity-mode nginx-mode edit-indirect which-key counsel-projectile dumb-jump multiple-cursors markdown-mode+ popup-kill-ring beacon htmlize ox-reveal ag swiper hydra yaml-mode smooth-scrolling smooth-scroll org-link-minor-mode elfeed projectile-rails pandoc ox-pandoc try expand-region hungry-delete org-bullets go-eldoc use-package)))
+    (ox-gfm solidity-mode nginx-mode edit-indirect which-key counsel-projectile dumb-jump multiple-cursors markdown-mode+ popup-kill-ring beacon htmlize ox-reveal ag swiper hydra yaml-mode smooth-scrolling smooth-scroll org-link-minor-mode elfeed projectile-rails pandoc ox-pandoc try expand-region hungry-delete org-bullets go-eldoc use-package)))
  '(pdf-view-midnight-colors (quote ("#969896" . "#f8eec7")))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
@@ -132,28 +131,28 @@
  '(vc-annotate-background-mode nil)
  '(vc-annotate-color-map
    (list
-	(cons 20 "#98be65")
-	(cons 40 "#b4be6c")
-	(cons 60 "#d0be73")
-	(cons 80 "#ECBE7B")
-	(cons 100 "#e6ab6a")
-	(cons 120 "#e09859")
-	(cons 140 "#da8548")
-	(cons 160 "#d38079")
-	(cons 180 "#cc7cab")
-	(cons 200 "#c678dd")
-	(cons 220 "#d974b7")
-	(cons 240 "#ec7091")
-	(cons 260 "#ff6c6b")
-	(cons 280 "#cf6162")
-	(cons 300 "#9f585a")
-	(cons 320 "#6f4e52")
-	(cons 340 "#5B6268")
-	(cons 360 "#5B6268")))
+    (cons 20 "#98be65")
+    (cons 40 "#b4be6c")
+    (cons 60 "#d0be73")
+    (cons 80 "#ECBE7B")
+    (cons 100 "#e6ab6a")
+    (cons 120 "#e09859")
+    (cons 140 "#da8548")
+    (cons 160 "#d38079")
+    (cons 180 "#cc7cab")
+    (cons 200 "#c678dd")
+    (cons 220 "#d974b7")
+    (cons 240 "#ec7091")
+    (cons 260 "#ff6c6b")
+    (cons 280 "#cf6162")
+    (cons 300 "#9f585a")
+    (cons 320 "#6f4e52")
+    (cons 340 "#5B6268")
+    (cons 360 "#5B6268")))
  '(vc-annotate-very-old-color nil)
  '(weechat-color-list
    (quote
-	(unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
+    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
  '(winner-mode t)
  '(xterm-color-names
    ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
